35b610f261eee0d132112ddbd1d1256f6870e35e
Normurodov Umarjon <userpeaseonly@gmail.com>
2025-06-14 19:20:24 +0500

    updated

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ async def receive_event(
 
         # Parse and log event
         try:
-            event = TypeAdapter(EventUnion).validate_python(**event_data, from_attributes=True)
+            event = TypeAdapter(EventUnion).validate_python(event_data)
             if isinstance(event, HeartbeatInfo):
                 logger.info(event)
             elif isinstance(event, EventNotificationAlert):
