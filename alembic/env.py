d0183dc799308353a23ca62999bab1b394530b00
Normurodov Umarjon <userpeaseonly@gmail.com>
2025-06-17 03:16:37 +0500

    updated

--- alembic/env.py
+++ alembic/env.py
@@ -6,6 +6,7 @@ from alembic import context
 
 
 from db import Base
+from models.event import Event, Heartbeat
 from core import config as settings
 
 import os
@@ -37,6 +38,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
+
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
